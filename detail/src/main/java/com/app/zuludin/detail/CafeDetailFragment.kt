5c41b66a7defa5a31854804642a74788027d0bc6
zuludin04 <zmauludin04@gmail.com>
2019-12-29 23:25:10 +0700

    Refactor detail layout

--- detail/src/main/java/com/app/zuludin/detail/CafeDetailFragment.kt
+++ detail/src/main/java/com/app/zuludin/detail/CafeDetailFragment.kt
@@ -1,23 +1,18 @@
 package com.app.zuludin.detail
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.zuludin.common.base.BaseFragment
 import com.app.zuludin.common.base.BaseViewModel
-import com.app.zuludin.common.loadUrlImage
 import com.app.zuludin.detail.adapter.CafeSlideAdapter
 import com.app.zuludin.detail.adapter.FacilityAdapter
 import com.app.zuludin.detail.databinding.FragmentCafeDetailBinding
@@ -55,15 +50,14 @@ class CafeDetailFragment : BaseFragment() {
         val cafe = args.detailCafe
         val facilityAdapter = FacilityAdapter(ArrayList())
 
-        recycler_more.apply {
-            layoutManager = GridLayoutManager(requireContext(), 2)
+        dataBinding.recyclerMore.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = facilityAdapter
         }
 
         viewModel.cafe.observe(this, Observer {
             it?.let { cafe ->
-                changeRatingColor(detail_cafe_rating, cafe.userRating?.ratingColor.toString())
-                detail_cafe_image.loadUrlImage(it.featuredImage)
                 detail_cafe_slide.adapter = cafe.photos?.let { photo -> CafeSlideAdapter(photo) }
                 detail_cafe_map.setOnClickListener {
                     openMap(cafe.location?.latitude, cafe.location?.longitude)
@@ -77,13 +71,6 @@ class CafeDetailFragment : BaseFragment() {
     }
 
     override fun getViewModel(): BaseViewModel = viewModel
-
-    private fun changeRatingColor(textView: TextView, color: String) {
-        textView.setTextColor(Color.parseColor("#$color"))
-        val drawable = textView.compoundDrawables[0].mutate()
-        drawable.colorFilter =
-            PorterDuffColorFilter(Color.parseColor("#$color"), PorterDuff.Mode.SRC_IN)
-    }
 
     private fun openMap(latitude: String?, longitude: String?) {
         val uri = String.format(
--- detail/src/main/java/com/app/zuludin/detail/view/DetailBinding.kt
+++ detail/src/main/java/com/app/zuludin/detail/view/DetailBinding.kt
@@ -1,9 +1,12 @@
 package com.app.zuludin.detail.view
 
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.agrawalsuneet.squareloaderspack.loaders.WaveLoader
+import com.app.zuludin.data.model.detail.DetailCafeResponse
 import com.app.zuludin.data.utils.Resource
+import com.app.zuludin.detail.R
 
 object DetailBinding {
     @BindingAdapter("app:showLoadingProgress")
@@ -11,6 +14,15 @@ object DetailBinding {
     fun showLoadingProgress(waveLoader: WaveLoader, status: Resource.Status?) {
         status?.let {
             waveLoader.visibility = if (it == Resource.Status.LOADING) View.VISIBLE else View.GONE
+        }
+    }
+
+    @BindingAdapter("app:averageCost")
+    @JvmStatic
+    fun setAverageCost(textView: TextView, detail: DetailCafeResponse?) {
+        detail?.let {
+            val average = textView.context.getString(R.string.cost, it.currency, it.averageCostForTwo.toString())
+            textView.text = average
         }
     }
 }
